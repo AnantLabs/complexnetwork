8fb4855d6a8045c2019ef14301d88a1860bd97f5
edvardgzraryan@gmail.com <edvardgzraryan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2012-03-25 20:42:36 +0000

    Improved ERModel structure, supported logging ability for ERModel.

--- trunk/Complex Network/ERmodel/Model/Realization/ERAnalyzer.cs
+++ trunk/Complex Network/ERmodel/Model/Realization/ERAnalyzer.cs
@@ -2,16 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
-using RandomGraph.Common.Model;
 using Model.ERModel.Result;
 using Model.ERModel.Realization;
+using CommonLibrary.Model;
+using log4net;
 
 namespace model.ERModel.Realization
 {
-    class ERAnalyzer
+    public class ERAnalyzer : IGraphAnalyzer
     {
+        /// <summary>
+        /// The logger static object for monitoring.
+        /// </summary>
+        protected static readonly ILog log = log4net.LogManager.GetLogger(typeof(ERAnalyzer));
+
         private ERContainer m_container;
         private AnalyzeResult m_result;
         private int[] m_minimal_path_list;
@@ -29,41 +34,59 @@ namespace model.ERModel.Realization
         
         public ERAnalyzer(ERContainer c)
         {
+            log.Info("Creating ERAnalizer object");
             m_container = c;
             m_result = new AnalyzeResult();
             m_minimal_path_list = new int[m_container.Size];
         }
 
-        public AnalyzeResult Analyze(AnalyseOptions options)
+        public void bfs(int s)
         {
-            if (((options & AnalyseOptions.AveragePath) == AnalyseOptions.AveragePath) ||
-                ((options & AnalyseOptions.Diameter) == AnalyseOptions.Diameter))
+            Queue<int> queue = new Queue<int>();
+            Node[] nodes = new Node[m_container.Size];
+            for (int i = 0; i < m_container.Size; ++i)
             {
-                count_essential_options();
-            }
-            if ((options & AnalyseOptions.ClusteringCoefficient) == AnalyseOptions.ClusteringCoefficient)
-            {
-                count_graph_clustering_coefficiant();
-            }
-            if ((options & AnalyseOptions.DegreeDistribution) == AnalyseOptions.DegreeDistribution)
-            {
-                count_degree_destribution();
-            }
-            if ((options & AnalyseOptions.Cycles3) == AnalyseOptions.Cycles3)
-            {
-                count_cycles_of_order3();
+                m_minimal_path_list[i] = -1;
+                nodes[i] = new Node();
             }
 
-            if ((options & AnalyseOptions.Cycles4) == AnalyseOptions.Cycles4)
-            {
-                count_cycles_of_order4();
-            }
-            //if ((options & AnalyseOptions.FullSubGraph) == AnalyseOptions.FullSubGraph)
-            //{
-            //    count_max_full_graphs();
-            //}
+            nodes[s].n_length = 0;
+            nodes[s].n_visited = true;
 
-            return m_result;
+            m_minimal_path_list[s] = 0;
+
+            queue.Enqueue(s);
+            while (queue.Count != 0)
+            {
+                int t = queue.Dequeue();
+                List<int> tmp = m_container.Neighbourship[t];
+                for (int i = 0; i < tmp.Count; ++i)
+                {
+                    int e = tmp[i];
+                    if (nodes[e].n_visited == false)
+                    {
+                        nodes[e].n_visited = true;
+                        nodes[e].n_length = nodes[t].n_length + 1;
+                        queue.Enqueue(e);
+                        m_minimal_path_list[e] = nodes[e].n_length;
+                    }
+                }
+            }
+        }
+
+        public double GetAveragePath()
+        {
+            return m_result.m_avgPathLenght;
+        }
+
+        public SortedDictionary<int, int> GetMinPathDist()
+        {
+            return m_result.m_pathDistribution;
+        }
+
+        public double GetDiameter()
+        {
+            return m_result.m_diameter;
         }
 
         public void count_essential_options()
@@ -96,42 +119,26 @@ namespace model.ERModel.Realization
                 }
             }
 
+            for (int i = 0; i < size; ++i)
+            {
+                if (m_result.m_pathDistribution.ContainsKey(i))
+                {
+                    m_result.m_pathDistribution[i] /= 2;
+                }
+            }
+
             m_result.m_diameter = d;
             m_result.m_avgPathLenght = Math.Round((double)sum / count, 4);
         }
 
-        public void bfs(int s)
+        public SortedDictionary<int, int> GetDegreeDistribution()
         {
-            Queue<int> queue = new Queue<int>();
-            Node[] nodes = new Node[m_container.Size];
-            for (int i = 0; i < m_container.Size; ++i)
-            {
-                m_minimal_path_list[i] = -1;
-                nodes[i] = new Node();
-            }
+            return m_result.m_degreeDistribution;
+        }
 
-            nodes[s].n_length = 0;
-            nodes[s].n_visited = true;
-
-            m_minimal_path_list[s] = 0;
-          
-            queue.Enqueue(s);
-            while (queue.Count != 0)
-            {
-                int t = queue.Dequeue();
-                List<int> tmp = m_container.Neighbourship[t];
-                for (int i = 0; i < tmp.Count; ++i)
-                {
-                    int e = tmp[i];
-                    if (nodes[e].n_visited == false)
-                    {
-                        nodes[e].n_visited = true;
-                        nodes[e].n_length = nodes[t].n_length + 1;
-                        queue.Enqueue(e);
-                        m_minimal_path_list[e] = nodes[e].n_length;
-                    }
-                }
-            }
+        public double get_average_degree()
+        {
+            return m_result.m_avgDegree;
         }
 
         public void count_degree_destribution()
@@ -155,6 +162,11 @@ namespace model.ERModel.Realization
             m_result.m_avgDegree = (double) avg / m_container.Size;
         }
 
+        public double GetCycles3()
+        {
+            return m_result.m_cyclesOfOrder3;
+        }
+
         public void count_cycles_of_order3()
         {
             int count = 0;
@@ -169,6 +181,11 @@ namespace model.ERModel.Realization
             }
 
             m_result.m_cyclesOfOrder3 = count / 6;
+        }
+
+        public double GetCycles4()
+        {
+            return m_result.m_cyclesOfOrder4;
         }
 
         public void count_cycles_of_order4()
@@ -196,7 +213,17 @@ namespace model.ERModel.Realization
             m_result.m_cyclesOfOrder4 = count / 8;
         }
 
-        public void count_graph_clustering_coefficiant()
+        public SortedDictionary<double, int> GetClusteringCoefficient()
+        {
+            return m_result.m_vertexClusteringCoefficient;
+        }
+
+        public double GetAvgClusteringCoefficient()
+        {
+            return m_result.m_clusteringCoefficient;
+        }
+
+        public void count_graph_clustering_coefficient()
         {
             double r = 0.0;
             double count = 0.0;
@@ -243,6 +270,46 @@ namespace model.ERModel.Realization
             }
 
             return (double)(2 * (count / 2 + neighbor_count)) / (neighbor_count * (neighbor_count + 1));
+        }
+
+        //Calculate distribution of connected subgraph of graph.
+        public SortedDictionary<int, int> GetConnSubGraph()
+        {
+            return new SortedDictionary<int, int>();
+        }
+        //Calculate count of cycles in 3 lenght based in eigen valu of graph.
+        public double GetCycleEigen3()
+        {
+            return 0;
+        }
+
+        //Calculate count of cycles in 4 lenght based in eigen valu of graph.
+        public double GetCycleEigen4()
+        {
+            return 0;
+        }
+        //Calculate motive of graph.
+        public void GetMotif()
+        {
+        }
+        //Calculate distribution of eigen value of graph.
+        public SortedDictionary<double, int> GetDistEigenPath()
+        {
+            return new SortedDictionary<double, int>();
+        }
+        public int GetMaxFullSubgraph()
+        {
+            return 0;
+        }
+        //Calculate distribution of connected subgraph of graph.
+        public SortedDictionary<int, int> GetFullSubGraph()
+        {
+            return new SortedDictionary<int, int>();
+        }
+        //Calculate Eigen values of graph.
+        public ArrayList GetEigenValue()
+        {
+            return new ArrayList();
         }
 
     }
