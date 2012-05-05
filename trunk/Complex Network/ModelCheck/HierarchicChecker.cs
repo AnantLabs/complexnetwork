5a5a02dd9e5342dec2e7a6bdcd786796d0fbd93c
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2012-05-05 19:23:18 +0000

    Updated model checker (temporary version).

--- trunk/Complex Network/ModelCheck/HierarchicChecker.cs
+++ trunk/Complex Network/ModelCheck/HierarchicChecker.cs
@@ -2,38 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace ModelCheck
 {
     public enum PossibleAnswers
     {
-        Yes,
-        No,
-        MaybeNo
+        GraphicalNotBlockHierarchic,
+        GraphicalBlockHierarchic,
+        NotGraphical,
+        MaybeNotBlockHierarchic
     }
+
 
     public class HierarchicChecker
     {
         private List<int> d;
         private int p;
         private int t;
+        private string path;
 
         public HierarchicChecker(List<int> t)
         {
             d = t;
         }
 
+        public HierarchicChecker(string filePath)
+        {
+            path = filePath;
+        }
+
         public PossibleAnswers IsHierarchic()
         {
-            CalcParms();
+            if (path != "")
+                d = FromMatrixToDegrees();
+            CalcParams();
             return Process(Polynom());
         }
 
-        private void CalcParms()
+        // Utilities //
+
+        private void CalcParams()
         {
             int k;
 
             for (p = 2; ; p++)
+            {
                 for (t = 1; ; t++)
                 {
                     k = (int)Math.Pow(p, t);
@@ -44,7 +58,7 @@ namespace ModelCheck
                     if (k > d.Count)
                         break;
                 }
-
+            }
         }
 
         private List<List<int>> Polynom()
@@ -64,12 +78,27 @@ namespace ModelCheck
             k.Sort(Comparing);
 
             return k;
+        }
 
+        private static int Comparing(List<int> k1, List<int> k2)
+        {
+            if (k1.Count != k2.Count)
+                throw new SystemException();
+
+            for (int i = k1.Count - 1; i >= 0; i--)
+                if (k1[i] != k2[i])
+                    return k1[i] - k2[i];
+
+            return 0;
         }
 
         private PossibleAnswers Process(List<List<int>> k)
         {
+            if (!HavelHakimiCheck(d))
+                return PossibleAnswers.NotGraphical;
+
             List<int> subk = new List<int>();
+
             List<int> subk1 = new List<int>();
 
             for (int l = 1; l <= t; l++) // makardakner@
@@ -86,12 +115,12 @@ namespace ModelCheck
                         {
                             if (k[j][i] != k[j + prevLevelElemCount][i])
                             {
-                                return PossibleAnswers.No;
+                                return PossibleAnswers.GraphicalNotBlockHierarchic;
                             }
                         }
-
                         subk1.Add(k[(h - 1) * levelElemCount][i]);
                     }
+
 
                     for (int j = (h - 1) * levelElemCount; j < h * levelElemCount; j += prevLevelElemCount)
                         subk.Add(k[j][l - 1]);
@@ -118,51 +147,29 @@ namespace ModelCheck
                                 for (int i = l; i < t; i++)
                                     if (subk1[i - 1] != k[v][i])
                                         chk = false;
-
                         }
                         if (chk == false)
-                            return PossibleAnswers.No;
+                            return PossibleAnswers.GraphicalNotBlockHierarchic;
                         if (localLev == 1 && chk == true)
-                            return PossibleAnswers.No;
-                        return PossibleAnswers.MaybeNo;
+                            return PossibleAnswers.GraphicalNotBlockHierarchic;
+                        return PossibleAnswers.MaybeNotBlockHierarchic;
                     }
                 }
             }
 
-            return PossibleAnswers.Yes;
-        }
-
-        private static int Comparing(List<int> k1, List<int> k2)
-        {
-            if (k1.Count != k2.Count)
-                throw new SystemException();
-
-            for (int i = k1.Count - 1; i >= 0; i--)
-                if (k1[i] != k2[i])
-                    return k1[i] - k2[i];
-
-            return 0;
-        }
-
-        private static List<int> HavelHakimi(List<int> d)
-        {
-            List<int> result = new List<int>();
-            for (int i = 0; i < d[0]; i++)
-                result.Add(d[i + 1] - 1);
-            for (int i = d[0] + 1; i < d.Count; i++)
-                result.Add(d[i]);
-            return result;
-
+            return PossibleAnswers.GraphicalBlockHierarchic;
         }
 
         private static bool HavelHakimiCheck(List<int> d)
         {
             List<int> f = new List<int>();
+            List<int> copy_of_d = new List<int>();
+            copy_of_d = d;
             for (int i = 0; i < d.Count; i++)
                 if (d[i] >= d.Count || d[i] < 0)
                     return false;
 
-            for (int i = 0; i < d.Count; i++)
+            for (int i = 0; i < copy_of_d.Count; i++)
             {
                 d.Sort();
                 d.Reverse();
@@ -175,5 +182,53 @@ namespace ModelCheck
                     return false;
             return true;
         }
+
+        private static List<int> HavelHakimi(List<int> d)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < d[0]; i++)
+                result.Add(d[i + 1] - 1);
+            for (int i = d[0] + 1; i < d.Count; i++)
+                result.Add(d[i]);
+            return result;
+        }   
+
+        private List<int> FromMatrixToDegrees() // matricic stanum e hajordakanutyun@
+        {
+            int sum = 0;
+            List<string> lines = new List<string>();
+            char[] lines1 = new char[100];
+            int[] lines2 = new int[100];
+
+            List<int> deg = new List<int>();
+
+            using (TextReader r = new StreamReader(path))
+            {
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                lines1 = lines[i].ToCharArray();
+
+                for (int j = 0; j < lines1.Length; j++)
+                {
+                    if (lines1[j] != ' ')
+                        lines2[j] = (int)(lines1[j] - '0');
+
+                    sum += lines2[j];
+                }
+
+                deg.Add(sum);
+                sum = 0;
+            }
+
+            return deg;
+        }
     }
+
 }
--- trunk/Complex Network/RandomGraphLauncher/ModelCheckWindow.Designer.cs
+++ trunk/Complex Network/RandomGraphLauncher/ModelCheckWindow.Designer.cs
@@ -35,6 +35,18 @@
             this.checkBtn = new System.Windows.Forms.Button();
             this.result = new System.Windows.Forms.Label();
             this.resultTxt = new System.Windows.Forms.TextBox();
+            this.dataGrp = new System.Windows.Forms.GroupBox();
+            this.degreesRadio = new System.Windows.Forms.RadioButton();
+            this.fromFileRadio = new System.Windows.Forms.RadioButton();
+            this.checkGrp = new System.Windows.Forms.GroupBox();
+            this.exactCheckRadio = new System.Windows.Forms.RadioButton();
+            this.notExactCheckRadio = new System.Windows.Forms.RadioButton();
+            this.filePathTxt = new System.Windows.Forms.TextBox();
+            this.filePath = new System.Windows.Forms.Label();
+            this.browse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGrp.SuspendLayout();
+            this.checkGrp.SuspendLayout();
             this.SuspendLayout();
             // 
             // modelName
@@ -53,11 +65,12 @@
             this.modelNameCmb.Name = "modelNameCmb";
             this.modelNameCmb.Size = new System.Drawing.Size(199, 21);
             this.modelNameCmb.TabIndex = 1;
+            this.modelNameCmb.SelectedIndexChanged += new System.EventHandler(this.modelNameCmb_SelectedIndexChanged);
             // 
             // degrees
             // 
             this.degrees.AutoSize = true;
-            this.degrees.Location = new System.Drawing.Point(12, 66);
+            this.degrees.Location = new System.Drawing.Point(99, 30);
             this.degrees.Name = "degrees";
             this.degrees.Size = new System.Drawing.Size(134, 13);
             this.degrees.TabIndex = 2;
@@ -65,15 +78,15 @@
             // 
             // degreesTxt
             // 
-            this.degreesTxt.Location = new System.Drawing.Point(15, 82);
+            this.degreesTxt.Location = new System.Drawing.Point(102, 46);
             this.degreesTxt.Multiline = true;
             this.degreesTxt.Name = "degreesTxt";
-            this.degreesTxt.Size = new System.Drawing.Size(196, 63);
+            this.degreesTxt.Size = new System.Drawing.Size(431, 60);
             this.degreesTxt.TabIndex = 3;
             // 
             // checkBtn
             // 
-            this.checkBtn.Location = new System.Drawing.Point(15, 169);
+            this.checkBtn.Location = new System.Drawing.Point(6, 54);
             this.checkBtn.Name = "checkBtn";
             this.checkBtn.Size = new System.Drawing.Size(196, 30);
             this.checkBtn.TabIndex = 4;
@@ -84,7 +97,7 @@
             // result
             // 
             this.result.AutoSize = true;
-            this.result.Location = new System.Drawing.Point(12, 221);
+            this.result.Location = new System.Drawing.Point(232, 57);
             this.result.Name = "result";
             this.result.Size = new System.Drawing.Size(37, 13);
             this.result.TabIndex = 5;
@@ -92,23 +105,126 @@
             // 
             // resultTxt
             // 
-            this.resultTxt.Location = new System.Drawing.Point(53, 218);
+            this.resultTxt.Location = new System.Drawing.Point(275, 54);
             this.resultTxt.Name = "resultTxt";
             this.resultTxt.ReadOnly = true;
-            this.resultTxt.Size = new System.Drawing.Size(158, 20);
+            this.resultTxt.Size = new System.Drawing.Size(258, 20);
             this.resultTxt.TabIndex = 6;
             this.resultTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // dataGrp
+            // 
+            this.dataGrp.Controls.Add(this.browse);
+            this.dataGrp.Controls.Add(this.filePath);
+            this.dataGrp.Controls.Add(this.filePathTxt);
+            this.dataGrp.Controls.Add(this.fromFileRadio);
+            this.dataGrp.Controls.Add(this.degreesRadio);
+            this.dataGrp.Controls.Add(this.degrees);
+            this.dataGrp.Controls.Add(this.degreesTxt);
+            this.dataGrp.Location = new System.Drawing.Point(12, 52);
+            this.dataGrp.Name = "dataGrp";
+            this.dataGrp.Size = new System.Drawing.Size(552, 156);
+            this.dataGrp.TabIndex = 7;
+            this.dataGrp.TabStop = false;
+            this.dataGrp.Text = "Data";
+            // 
+            // degreesRadio
+            // 
+            this.degreesRadio.AutoSize = true;
+            this.degreesRadio.Location = new System.Drawing.Point(6, 28);
+            this.degreesRadio.Name = "degreesRadio";
+            this.degreesRadio.Size = new System.Drawing.Size(65, 17);
+            this.degreesRadio.TabIndex = 0;
+            this.degreesRadio.TabStop = true;
+            this.degreesRadio.Text = "Degrees";
+            this.degreesRadio.UseVisualStyleBackColor = true;
+            this.degreesRadio.CheckedChanged += new System.EventHandler(this.degreesRadio_CheckedChanged);
+            // 
+            // fromFileRadio
+            // 
+            this.fromFileRadio.AutoSize = true;
+            this.fromFileRadio.Location = new System.Drawing.Point(6, 116);
+            this.fromFileRadio.Name = "fromFileRadio";
+            this.fromFileRadio.Size = new System.Drawing.Size(67, 17);
+            this.fromFileRadio.TabIndex = 1;
+            this.fromFileRadio.TabStop = true;
+            this.fromFileRadio.Text = "From File";
+            this.fromFileRadio.UseVisualStyleBackColor = true;
+            this.fromFileRadio.CheckedChanged += new System.EventHandler(this.fromFileRadio_CheckedChanged);
+            // 
+            // checkGrp
+            // 
+            this.checkGrp.Controls.Add(this.notExactCheckRadio);
+            this.checkGrp.Controls.Add(this.exactCheckRadio);
+            this.checkGrp.Controls.Add(this.resultTxt);
+            this.checkGrp.Controls.Add(this.checkBtn);
+            this.checkGrp.Controls.Add(this.result);
+            this.checkGrp.Location = new System.Drawing.Point(12, 224);
+            this.checkGrp.Name = "checkGrp";
+            this.checkGrp.Size = new System.Drawing.Size(552, 115);
+            this.checkGrp.TabIndex = 8;
+            this.checkGrp.TabStop = false;
+            this.checkGrp.Text = "Check";
+            // 
+            // exactCheckRadio
+            // 
+            this.exactCheckRadio.AutoSize = true;
+            this.exactCheckRadio.Location = new System.Drawing.Point(6, 19);
+            this.exactCheckRadio.Name = "exactCheckRadio";
+            this.exactCheckRadio.Size = new System.Drawing.Size(86, 17);
+            this.exactCheckRadio.TabIndex = 0;
+            this.exactCheckRadio.TabStop = true;
+            this.exactCheckRadio.Text = "Exact Check";
+            this.exactCheckRadio.UseVisualStyleBackColor = true;
+            // 
+            // notExactCheckRadio
+            // 
+            this.notExactCheckRadio.AutoSize = true;
+            this.notExactCheckRadio.Location = new System.Drawing.Point(102, 19);
+            this.notExactCheckRadio.Name = "notExactCheckRadio";
+            this.notExactCheckRadio.Size = new System.Drawing.Size(106, 17);
+            this.notExactCheckRadio.TabIndex = 1;
+            this.notExactCheckRadio.TabStop = true;
+            this.notExactCheckRadio.Text = "Not Exact Check";
+            this.notExactCheckRadio.UseVisualStyleBackColor = true;
+            // 
+            // filePathTxt
+            // 
+            this.filePathTxt.Location = new System.Drawing.Point(153, 113);
+            this.filePathTxt.Name = "filePathTxt";
+            this.filePathTxt.Size = new System.Drawing.Size(217, 20);
+            this.filePathTxt.TabIndex = 4;
+            // 
+            // filePath
+            // 
+            this.filePath.AutoSize = true;
+            this.filePath.Location = new System.Drawing.Point(99, 118);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(48, 13);
+            this.filePath.TabIndex = 5;
+            this.filePath.Text = "File Path";
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(391, 112);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(142, 23);
+            this.browse.TabIndex = 6;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // ModelCheckWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(226, 257);
-            this.Controls.Add(this.resultTxt);
-            this.Controls.Add(this.result);
-            this.Controls.Add(this.checkBtn);
-            this.Controls.Add(this.degreesTxt);
-            this.Controls.Add(this.degrees);
+            this.ClientSize = new System.Drawing.Size(576, 361);
+            this.Controls.Add(this.checkGrp);
+            this.Controls.Add(this.dataGrp);
             this.Controls.Add(this.modelNameCmb);
             this.Controls.Add(this.modelName);
             this.MaximizeBox = false;
@@ -117,6 +233,10 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Model Check Window";
             this.Load += new System.EventHandler(this.ModelCheckWindow_Load);
+            this.dataGrp.ResumeLayout(false);
+            this.dataGrp.PerformLayout();
+            this.checkGrp.ResumeLayout(false);
+            this.checkGrp.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,5 +251,15 @@
         private System.Windows.Forms.Button checkBtn;
         private System.Windows.Forms.Label result;
         private System.Windows.Forms.TextBox resultTxt;
+        private System.Windows.Forms.GroupBox dataGrp;
+        private System.Windows.Forms.RadioButton fromFileRadio;
+        private System.Windows.Forms.RadioButton degreesRadio;
+        private System.Windows.Forms.GroupBox checkGrp;
+        private System.Windows.Forms.RadioButton notExactCheckRadio;
+        private System.Windows.Forms.RadioButton exactCheckRadio;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.Label filePath;
+        private System.Windows.Forms.TextBox filePathTxt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
