d2a506d9bd83942705842e054c69905d19f25f89
ankocharyan@gmail.com <ankocharyan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2012-07-16 11:37:25 +0000

    Temporary version (no compile errors).

---
+++ Complex Network/CommonLibrary/Model/IGraphContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CommonLibrary.Model
+{
+    // Общий интерфейс для контейнера графа.
+    // Каждий контейнер графа любой модели должен реализовать этот интерфейс.
+    public interface IGraphContainer
+    {
+        // Размер контейнера (число вершин в графе).
+        int Size { get; set; }
+
+        // Строится граф на основе матрицы смежности.
+        void SetMatrix(ArrayList matrix);
+        // Возвращается матрица смежности, соответсвующая графу.
+        bool[,] GetMatrix();
+    }
+}
---
+++ Complex Network/CommonLibrary/Model/IGraphGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using RandomGraph.Common.Model.Generation;
+
+namespace CommonLibrary.Model
+{
+    // Общий интерфейс для генератора графа.
+    // Каждий генератор любой модели должен реализовать этот интерфейс.
+    public interface IGraphGenerator
+    {
+        // Контейнер, в котором содержится сгенерированный граф.
+        IGraphContainer Container { get; set; }
+
+        // Случайным образом генерируется граф, на основе параметров генерации.
+        void RandomGeneration(Dictionary<GenerationParam, object> genParam);
+        // Строится граф, на основе матрицы смежности.
+        void StaticGeneration(ArrayList matrix);
+    }
+}
