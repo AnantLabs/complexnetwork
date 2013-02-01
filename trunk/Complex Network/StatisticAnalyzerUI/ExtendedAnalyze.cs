e880caeab923c404777e05a80bd5e0f86903c31d
ankocharyan@gmail.com <ankocharyan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2013-02-01 16:18:20 +0000

    Extended Tool in Statistic Analyzer (corrected version).

---
+++ trunk/Complex Network/StatisticAnalyzerUI/ExtendedAnalyze.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+using CommonLibrary.Model.Result;
+using StatisticAnalyzer;
+using StatisticAnalyzer.Loader;
+
+namespace StatisticAnalyzerUI
+{
+    public partial class ExtendedAnalyze : Form
+    {
+        public ExtendedAnalyze()
+        {
+            InitializeComponent();
+        }
+
+        private void trajectoryAnalyze_Click(object sender, EventArgs e)
+        {
+            // !исправить!
+            StLoader loader = new StLoader();
+            loader.ModelName = "ERModel";
+            loader.InitAssemblies();
+            List<ResultAssembly> list = loader.SelectAllAssemblies();
+
+            SortedDictionary<BigInteger, double> avgs = new SortedDictionary<BigInteger,double>();
+            SortedDictionary<BigInteger, double> sigmas = new SortedDictionary<BigInteger,double>();
+
+            foreach (ResultAssembly resultAssembly in list)
+            {
+                SortedDictionary<double, double> r = new SortedDictionary<double, double>();
+                int instanceCount = resultAssembly.Results.Count();
+                for (int j = 0; j < instanceCount; ++j)
+                {
+                    SortedDictionary<int, double> tempDictionary = resultAssembly.Results[j].TriangleTrajectory;
+                    SortedDictionary<int, double>.KeyCollection keyColl = tempDictionary.Keys;
+                    foreach (int key in keyColl)
+                    {
+                        if (r.Keys.Contains(key))
+                            r[key] += tempDictionary[key];
+                        else
+                            r.Add(key, tempDictionary[key]);
+                    }
+                }
+
+                SortedDictionary<double, double> result = new SortedDictionary<double, double>();
+                SortedDictionary<double, double>.KeyCollection keys = r.Keys;
+                foreach (double key in keys)
+                {
+                    result.Add(key, r[key] / instanceCount);
+                }
+
+                SortedDictionary<double, double>.KeyCollection resultKeys = result.Keys;
+                double avg = 0, sigma = 0;
+
+                foreach (double key in resultKeys)
+                {
+                    avg += result[key];
+                }
+                avg /= resultKeys.Count();
+
+                foreach (double key in resultKeys)
+                {
+                    sigma += Math.Pow((avg - result[key]), 2); 
+                }
+                sigma /= resultKeys.Count();
+                sigma = Math.Sqrt(sigma);
+
+                avgs.Add(resultAssembly.Results[0].trajectoryMu, avg);
+                sigmas.Add(resultAssembly.Results[0].trajectoryMu, sigma);
+            }
+
+            ExtendedGraphic avgsGraphic = new ExtendedGraphic(avgs, "Average");
+            avgsGraphic.Show();
+
+            ExtendedGraphic sigmasGraphic = new ExtendedGraphic(sigmas, "Sigma");
+            sigmasGraphic.Show();            
+        }
+    }
+}
---
+++ trunk/Complex Network/StatisticAnalyzerUI/ExtendedGraphic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Numerics;
+
+using ZedGraph;
+
+namespace StatisticAnalyzerUI
+{
+    public partial class ExtendedGraphic : Form
+    {
+        string yAxis;
+        ZedGraphControl graphic;
+        PointPairList values;
+
+        public ExtendedGraphic(SortedDictionary<BigInteger, double> dict, string text)
+        {
+            yAxis = text;
+
+            graphic = new ZedGraphControl();
+            graphic.Dock = DockStyle.Fill;
+            this.Controls.Add(graphic);
+
+            values = new PointPairList();
+
+            SortedDictionary<BigInteger, double>.KeyCollection keys = dict.Keys;
+            foreach (BigInteger i in keys)
+            {
+                values.Add(Convert.ToDouble(i.ToString()), dict[i]);
+            }
+
+            InitializeComponent();
+        }
+
+        private void ExtendedGraphic_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + yAxis;
+
+            graphic.GraphPane.Title.Text = yAxis;
+            graphic.GraphPane.XAxis.Title.Text = "Mu";
+            graphic.GraphPane.YAxis.Title.Text = yAxis;
+
+            LineItem l = graphic.GraphPane.AddCurve(yAxis, values, Color.Black, SymbolType.Circle);
+
+            graphic.AxisChange();
+            graphic.Invalidate();
+            graphic.Refresh();
+        }
+    }
+}
