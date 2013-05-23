b494e08ba983461f36a510b4ff303e8c7eeebcab
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2013-05-23 19:08:00 +0000

    Added Analyze Result File converter to ResultStorage.

--- trunk/Complex Network/RandomGraphLauncher/DataExportImportWindow.designer.cs
+++ trunk/Complex Network/RandomGraphLauncher/DataExportImportWindow.designer.cs
@@ -47,6 +47,7 @@
             this.xmlLocationTxt = new System.Windows.Forms.TextBox();
             this.xmlBrowse = new System.Windows.Forms.Button();
             this.externalStoreGrp = new System.Windows.Forms.GroupBox();
+            this.avgCheck = new System.Windows.Forms.CheckBox();
             this.externalBrowse = new System.Windows.Forms.Button();
             this.externalLocation = new System.Windows.Forms.Label();
             this.externalLocationTxt = new System.Windows.Forms.TextBox();
@@ -56,7 +57,22 @@
             this.connectionStringTxt = new System.Windows.Forms.TextBox();
             this.Connections = new System.Windows.Forms.Button();
             this.fromFileXml = new System.Windows.Forms.Button();
-            this.avgCheck = new System.Windows.Forms.CheckBox();
+            this.externalStoreTab = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.mainTab.SuspendLayout();
@@ -65,6 +81,10 @@
             this.xmlStoreGrp.SuspendLayout();
             this.externalStoreGrp.SuspendLayout();
             this.sqlStoreGrp.SuspendLayout();
+            this.externalStoreTab.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.SuspendLayout();
             // 
             // LocationTxt
@@ -172,6 +192,7 @@
             // 
             this.mainTab.Controls.Add(this.xmlOrSqlStoreTab);
             this.mainTab.Controls.Add(this.externalStoreTTTab);
+            this.mainTab.Controls.Add(this.externalStoreTab);
             this.mainTab.Location = new System.Drawing.Point(12, 12);
             this.mainTab.Name = "mainTab";
             this.mainTab.SelectedIndex = 0;
@@ -259,6 +280,16 @@
             this.externalStoreGrp.TabStop = false;
             this.externalStoreGrp.Text = "External store";
             // 
+            // avgCheck
+            // 
+            this.avgCheck.AutoSize = true;
+            this.avgCheck.Location = new System.Drawing.Point(120, 56);
+            this.avgCheck.Name = "avgCheck";
+            this.avgCheck.Size = new System.Drawing.Size(66, 17);
+            this.avgCheck.TabIndex = 24;
+            this.avgCheck.Text = "Average";
+            this.avgCheck.UseVisualStyleBackColor = true;
+            // 
             // externalBrowse
             // 
             this.externalBrowse.Location = new System.Drawing.Point(276, 29);
@@ -344,15 +375,167 @@
             this.fromFileXml.UseVisualStyleBackColor = true;
             this.fromFileXml.Click += new System.EventHandler(this.fromFileXml_Click);
             // 
-            // avgCheck
+            // externalStoreTab
             // 
-            this.avgCheck.AutoSize = true;
-            this.avgCheck.Location = new System.Drawing.Point(120, 56);
-            this.avgCheck.Name = "avgCheck";
-            this.avgCheck.Size = new System.Drawing.Size(66, 17);
-            this.avgCheck.TabIndex = 24;
-            this.avgCheck.Text = "Average";
-            this.avgCheck.UseVisualStyleBackColor = true;
+            this.externalStoreTab.Controls.Add(this.groupBox3);
+            this.externalStoreTab.Controls.Add(this.groupBox4);
+            this.externalStoreTab.Controls.Add(this.button3);
+            this.externalStoreTab.Controls.Add(this.groupBox5);
+            this.externalStoreTab.Controls.Add(this.button5);
+            this.externalStoreTab.Location = new System.Drawing.Point(4, 22);
+            this.externalStoreTab.Name = "externalStoreTab";
+            this.externalStoreTab.Padding = new System.Windows.Forms.Padding(3);
+            this.externalStoreTab.Size = new System.Drawing.Size(434, 244);
+            this.externalStoreTab.TabIndex = 2;
+            this.externalStoreTab.Text = "External Store";
+            this.externalStoreTab.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(6, 89);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 80);
+            this.groupBox3.TabIndex = 36;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "XML store";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "XML Location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(96, 20);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.Text = "C:\\ComplexNetwork";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.checkBox1);
+            this.groupBox4.Controls.Add(this.button2);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Location = new System.Drawing.Point(6, 6);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(413, 77);
+            this.groupBox4.TabIndex = 32;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "External store";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(120, 56);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 24;
+            this.checkBox1.Text = "Average";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(276, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Directory location";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(120, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "C:\\ComplexNetwork";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(247, 183);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 46);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Import data from file into SQL datatbase";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Controls.Add(this.textBox3);
+            this.groupBox5.Controls.Add(this.button4);
+            this.groupBox5.Location = new System.Drawing.Point(219, 89);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 80);
+            this.groupBox5.TabIndex = 33;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "SQL store";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "ConnectionString";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(96, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(109, 37);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(82, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Connections";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.AddConnection_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 183);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 46);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "Import data from file into XML";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // DataExportImportWindow
             // 
@@ -376,6 +559,13 @@
             this.externalStoreGrp.PerformLayout();
             this.sqlStoreGrp.ResumeLayout(false);
             this.sqlStoreGrp.PerformLayout();
+            this.externalStoreTab.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -411,6 +601,22 @@
         private System.Windows.Forms.TextBox xmlLocationTxt;
         private System.Windows.Forms.Button xmlBrowse;
         private System.Windows.Forms.CheckBox avgCheck;
+        private System.Windows.Forms.TabPage externalStoreTab;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
 
     }
 }
