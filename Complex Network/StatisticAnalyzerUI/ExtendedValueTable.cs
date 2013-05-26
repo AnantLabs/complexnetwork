7a449f7872f05df23913870eafb513adce61a82f
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2013-05-26 10:30:24 +0000

    Corrected Value table in Extended Analyze (Statistic Analyzer).

--- Complex Network/StatisticAnalyzerUI/ExtendedValueTable.cs
+++ Complex Network/StatisticAnalyzerUI/ExtendedValueTable.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using CarlosAg.ExcelXmlWriter;
+
 namespace StatisticAnalyzerUI
 {
     public partial class ExtendedValueTable : Form
@@ -39,7 +41,66 @@ namespace StatisticAnalyzerUI
 
         private void excelButton_Click(object sender, EventArgs e)
         {
+            Workbook book = new Workbook();
+            // Some optional properties of the Document
+            book.Properties.Author = "Yerevan State University, Faculty of Informatics and Applied Mathematics," +
+                "Chair of Programming and Information Technologies";
+            book.Properties.Title = "Extended Value Table";
+            book.Properties.Created = DateTime.Now;
 
+            // Add some styles to the Workbook
+            WorksheetStyle style = book.Styles.Add("HeaderStyle");
+            style.Font.FontName = "TimesNewRoman";
+            style.Font.Size = 14;
+            style.Font.Bold = true;
+            style.Alignment.Horizontal = StyleHorizontalAlignment.Center;
+            style.Font.Color = "White";
+            style.Interior.Color = "Gray";
+            style.Interior.Pattern = StyleInteriorPattern.Solid;
+
+            style = book.Styles.Add("ColumnNames");
+            style.Font.FontName = "TimesNewRoman";
+            style.Font.Size = 12;
+            style.Font.Bold = true;
+            style.Alignment.Horizontal = StyleHorizontalAlignment.Center;
+            style.Font.Color = "White";
+            style.Interior.Color = "Gray";
+            style.Interior.Pattern = StyleInteriorPattern.Solid;
+
+            style = book.Styles.Add("Default");
+            style.Font.FontName = "TimesNewRoman";
+            style.Font.Size = 11;
+            style.Alignment.Horizontal = StyleHorizontalAlignment.Center;
+
+            Worksheet sheet = book.Worksheets.Add("Value Table");
+            sheet.Table.Columns.Add(new WorksheetColumn(300));
+            sheet.Table.Columns.Add(new WorksheetColumn(300));
+
+            WorksheetRow row = sheet.Table.Rows.Add();
+
+            WorksheetCell generationCell = new WorksheetCell(this.generationParametersTxt.Text, "HeaderStyle");
+            generationCell.Comment.Data.Text = this.generationParameters.Text;
+            row.Cells.Add(generationCell);
+            generationCell.MergeAcross = 1;
+
+            row = sheet.Table.Rows.Add();
+            row.Cells.Add(this.valuesGrd.Columns[0].HeaderText, DataType.String, "ColumnNames");
+            row.Cells.Add(this.valuesGrd.Columns[1].HeaderText, DataType.String, "ColumnNames");
+
+            for (int i = 0; i < this.valuesGrd.Rows.Count; i++)
+            {
+                row = sheet.Table.Rows.Add();
+                row.Cells.Add(new WorksheetCell(this.valuesGrd.Rows[i].Cells[0].Value.ToString(),
+                    DataType.String, "Default"));
+                row.Cells.Add(new WorksheetCell(this.valuesGrd.Rows[i].Cells[1].Value.ToString(),
+                    DataType.String, "Default"));
+            }
+
+            saveFileDialog.FileName = "Extended ValueTable.xls";
+            if (this.saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                book.Save(this.saveFileDialog.FileName);
+            }
         }
 
         private void Print_Click(object sender, EventArgs e)
