983d3ba12b446e16471358bfba7af7f989a08db0
ankocharyan@gmail.com <ankocharyan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2013-07-29 11:09:46 +0000

    Global changes (temporary version).

---
+++ Complex Network/CommonLibrary/Model/AbstractGraphContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CommonLibrary.Model
+{
+    // Базовый абстрактный класс для имплементации контейнера графа.
+    public abstract class AbstractGraphContainer
+    {        
+        // Размер контейнера (число вершин в графе).
+        public abstract int Size { get; set; }
+
+        // Строится граф на основе матрицы смежности.
+        public abstract void SetMatrix(string fileName);
+
+        // Возвращается матрица смежности, соответсвующая графу.
+        public abstract bool[,] GetMatrix();
+
+        // ??
+        public abstract int[][] GetBranches();
+    }
+}
