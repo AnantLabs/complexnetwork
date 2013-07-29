642f66d0457064a3be2c77432820d2e362d6e7a4
ankocharyan@gmail.com <ankocharyan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2013-07-29 11:09:46 +0000

    Global changes (temporary version).

---
+++ trunk/Complex Network/CommonLibrary/Model/AbstractGraphGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using RandomGraph.Common.Model.Generation;
+
+namespace CommonLibrary.Model
+{
+    // Базовый абстрактный класс для имплементации генератора графа.
+    public abstract class AbstractGraphGenerator
+    {
+        // Контейнер, в котором содержится сгенерированный граф.
+        public abstract AbstractGraphContainer Container { get; set; }
+
+        // Permanet Generation
+        public abstract void PermanentGeneration(Dictionary<GenerationParam, object> genParam);
+
+        // Случайным образом генерируется граф, на основе параметров генерации.
+        public void Generation(Dictionary<GenerationParam, object> genParam)
+        {
+            if (genParam.ContainsKey(GenerationParam.FileName))
+            {
+                // Статическая генерация.
+                StaticGeneration((string)genParam[GenerationParam.FileName]);                
+            }
+            else
+            {
+                // Динамическая генерация.
+                RandomGeneration(genParam);
+            }
+        }
+
+        protected abstract void RandomGeneration(Dictionary<GenerationParam, object> genParam);
+        protected abstract void StaticGeneration(string filePath);
+    }
+}
