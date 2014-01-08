6539c65ba4670ef5cb22a21a9aa2eaaa9581dc7b
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-01-08 10:55:49 +0000

    Changes in Percolation Research.

--- trunk/Complex Network/RandomGraphLauncher/Percolations.cs
+++ trunk/Complex Network/RandomGraphLauncher/Percolations.cs
@@ -252,6 +252,46 @@ namespace Percolations
             MessageBox.Show("Results are saved succesfully!");
         }
 
+        private void startSublevelsHierarchic_Click(object sender, EventArgs e)
+        {
+            ResultResearch result = new ResultResearch();
+            result.Name = this.jobName;
+            result.ModelType = typeof(HierarchicModel);
+            result.Delta = Double.Parse(this.deltaExtendedTxt.Text);
+            result.RealizationCount = (Int32)this.realizationCountNum.Value;
+            result.Function = this.probabilityFunctionCmb.Text;
+            result.GenerationParams[GenerationParam.BranchIndex] = Int16.Parse(this.branchIndexCmb.Text);
+            result.GenerationParams[GenerationParam.Level] = Int16.Parse(this.maxLevelCmb.Text);
+            result.Size = (int)Math.Pow(Int16.Parse(this.branchIndexCmb.Text), Int16.Parse(this.maxLevelCmb.Text));
+
+            Int16 maxLevel = Int16.Parse(this.maxLevelCmb.Text);
+            for (Int16 g = 1; g <= maxLevel; ++g)
+            {
+                result.Result.Add(g, new SortedDictionary<double, double>());
+            }
+
+            double muLow = Double.Parse(this.muRangeLowExtendedTxt.Text);
+            double muHigh = Double.Parse(this.muRangeHighExtendedTxt.Text);
+            double muDelta = Double.Parse(this.deltaExtendedTxt.Text);
+
+            double muTemp = muLow;
+            while (muTemp < muHigh)
+            {
+                SortedDictionary<double, double> r = AnalyzeSublevelsHierarchic(muTemp);
+                foreach (double gamma in r.Keys)
+                {
+                    result.Result[gamma].Add(muTemp, r[gamma]);
+                }
+
+                muTemp += muDelta;
+            }
+
+            XMLResultStorage storage = new XMLResultStorage(Options.StorageDirectory);
+            storage.SaveResearch(result);
+
+            MessageBox.Show("Results are saved succesfully!");
+        }
+
         // Утилиты.
 
         private void InitializeHierarchicModel()
@@ -269,6 +309,7 @@ namespace Percolations
             this.startHierarchic.Visible = true;
             this.startAvgHierarchic.Visible = true;
             this.startGlobal.Visible = true;
+            this.startSublevelsHierarchic.Visible = true;
         }
 
         private void InitializeERModel()
@@ -339,10 +380,11 @@ namespace Percolations
                 hGenerator.Generation(genParameters);
                 hAnalyzer = new HierarchicAnalyzer((HierarchicContainer)hGenerator.Container);
 
+                SortedDictionary<int, int> temp;
                 for (Int16 i = 1; i <= maxLevel; ++i)
                 {
-                    double maxOrder = (hAnalyzer.GetConnSubGraphPerLevel(i).Count != 0) ? 
-                        hAnalyzer.GetConnSubGraphPerLevel(i).Last().Key : 1;
+                    temp = hAnalyzer.GetConnSubGraphPerLevel(i);
+                    double maxOrder = (temp.Count != 0) ? temp.Last().Key : 1;
                     double normMaxOrder = maxOrder / (Math.Pow(p,i));
                     result[i] += normMaxOrder;
                 }
@@ -429,6 +471,49 @@ namespace Percolations
                 result.Add(qTemp, avgValue);
 
                 qTemp += qDelta;
+            }
+
+            return result;
+        }
+
+        private SortedDictionary<double, double> AnalyzeSublevelsHierarchic(double mu)
+        {
+            Int16 p = Int16.Parse(this.branchIndexCmb.Text);
+            Int16 maxLevel = Int16.Parse(this.maxLevelCmb.Text);
+            int realizationCount = (Int32)this.realizationCountNum.Value;
+
+            SortedDictionary<double, double> result = new SortedDictionary<double, double>();
+            for (Int16 i = 1; i <= maxLevel; ++i)
+            {
+                result.Add(i, 0);
+            }
+
+            Dictionary<GenerationParam, object> genParameters = new Dictionary<GenerationParam, object>();
+            genParameters.Add(GenerationParam.BranchIndex, p);
+            genParameters.Add(GenerationParam.Level, maxLevel);
+            genParameters.Add(GenerationParam.Mu, mu);
+
+            HierarchicGenerator hGenerator = new HierarchicGenerator();
+            HierarchicAnalyzer hAnalyzer;
+
+            for (int r = 0; r < realizationCount; ++r)
+            {
+                hGenerator.Generation(genParameters);
+                hAnalyzer = new HierarchicAnalyzer((HierarchicContainer)hGenerator.Container);
+
+                SortedDictionary<int, int> temp;
+                for (Int16 i = 1; i <= maxLevel; ++i)
+                {
+                    temp = hAnalyzer.GetConnSubGraphSublevels(i);
+                    double maxOrder = (temp.Count != 0) ? temp.Last().Key : 1;
+                    double normMaxOrder = maxOrder / (Math.Pow(p, i));
+                    result[i] += normMaxOrder;
+                }
+            }
+
+            for (Int16 i = 1; i <= maxLevel; ++i)
+            {
+                result[i] /= realizationCount;
             }
 
             return result;
