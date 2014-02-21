576228c660d52c4d68f5e72441b7e837943855ca
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-02-21 11:15:51 +0000

    Removed renamed files.

---
+++ Random Networks Explorer/Core/AbstractEnsembleManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Core.Enumerations;
+
+namespace Core
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class AbstractEnsembleManager
+    {
+        protected ModelType modelType;
+        protected string tracingPath;
+        protected int realizationCount;
+
+        protected Dictionary<GenerationParameter, object> generationParameterValues;
+        protected AnalyzeOption analyzeOptions;
+
+        public ModelType ModelType
+        {
+            set { modelType = value; }
+        }
+
+        public string TracingPath
+        {
+            set { tracingPath = value; }
+        }
+
+        public int RealizationCount
+        {
+            set { realizationCount = value; }
+        }
+
+        public Dictionary<GenerationParameter, object> GenerationParameterValues
+        {
+            set { generationParameterValues = value; }
+        }
+
+        public AnalyzeOption AnalyzeOptions
+        {
+            set { analyzeOptions = value; }
+        }
+
+        public abstract void Run();
+        public abstract void Cancel();
+    }
+}
