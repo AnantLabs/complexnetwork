4ca6c468e5ed402eda08a70b5e7bfab7ef987006
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-02-21 11:15:51 +0000

    Removed renamed files.

--- trunk/Random Networks Explorer/Core/AbstractNetwork.cs
+++ trunk/Random Networks Explorer/Core/AbstractNetwork.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Core.Model
+namespace Core
 {
-    class AbstractNetworkGenerator
+    public class AbstractNetwork
     {
     }
 }
