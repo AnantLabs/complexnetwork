0b4e317f362d96a9c9a55feed882224e062e58c8
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-04-29 09:56:40 +0000

    Changes in GUI.

--- Random Networks Explorer/Random Networks Explorer/StorageSettingsWindow.Designer.cs
+++ Random Networks Explorer/Random Networks Explorer/StorageSettingsWindow.Designer.cs
@@ -28,114 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.storageGroupBox = new System.Windows.Forms.GroupBox();
-            this.Browse = new System.Windows.Forms.Button();
-            this.SQLRadioButton = new System.Windows.Forms.RadioButton();
-            this.AddConnection = new System.Windows.Forms.Button();
-            this.textBoxConnStr = new System.Windows.Forms.TextBox();
-            this.XMLRadioButton = new System.Windows.Forms.RadioButton();
-            this.LocationTxt = new System.Windows.Forms.TextBox();
-            this.LabelConnStr = new System.Windows.Forms.Label();
-            this.location = new System.Windows.Forms.Label();
             this.cancelButton = new System.Windows.Forms.Button();
             this.okButton = new System.Windows.Forms.Button();
-            this.storageGroupBox.SuspendLayout();
+            this.xmlBrowseButton = new System.Windows.Forms.Button();
+            this.sqlRadioButton = new System.Windows.Forms.RadioButton();
+            this.connectionsButton = new System.Windows.Forms.Button();
+            this.connectionStrTxt = new System.Windows.Forms.TextBox();
+            this.xmlRadioButton = new System.Windows.Forms.RadioButton();
+            this.xmlOutputDirectoryTxt = new System.Windows.Forms.TextBox();
+            this.connectionStr = new System.Windows.Forms.Label();
+            this.xmlOutputDirectory = new System.Windows.Forms.Label();
+            this.txtBrowseButton = new System.Windows.Forms.Button();
+            this.txtRadioButton = new System.Windows.Forms.RadioButton();
+            this.txtOutputDirectoryTxt = new System.Windows.Forms.TextBox();
+            this.txtOutputDirectory = new System.Windows.Forms.Label();
+            this.browserDlg = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
-            // 
-            // storageGroupBox
-            // 
-            this.storageGroupBox.Controls.Add(this.Browse);
-            this.storageGroupBox.Controls.Add(this.SQLRadioButton);
-            this.storageGroupBox.Controls.Add(this.AddConnection);
-            this.storageGroupBox.Controls.Add(this.textBoxConnStr);
-            this.storageGroupBox.Controls.Add(this.XMLRadioButton);
-            this.storageGroupBox.Controls.Add(this.LocationTxt);
-            this.storageGroupBox.Controls.Add(this.LabelConnStr);
-            this.storageGroupBox.Controls.Add(this.location);
-            this.storageGroupBox.Location = new System.Drawing.Point(12, 12);
-            this.storageGroupBox.Name = "storageGroupBox";
-            this.storageGroupBox.Size = new System.Drawing.Size(421, 171);
-            this.storageGroupBox.TabIndex = 1;
-            this.storageGroupBox.TabStop = false;
-            this.storageGroupBox.Text = "Choose data store";
-            // 
-            // Browse
-            // 
-            this.Browse.Location = new System.Drawing.Point(315, 59);
-            this.Browse.Name = "Browse";
-            this.Browse.Size = new System.Drawing.Size(82, 23);
-            this.Browse.TabIndex = 22;
-            this.Browse.Text = "Browse";
-            this.Browse.UseVisualStyleBackColor = true;
-            // 
-            // SQLRadioButton
-            // 
-            this.SQLRadioButton.AutoSize = true;
-            this.SQLRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.SQLRadioButton.Location = new System.Drawing.Point(27, 92);
-            this.SQLRadioButton.Name = "SQLRadioButton";
-            this.SQLRadioButton.Size = new System.Drawing.Size(72, 17);
-            this.SQLRadioButton.TabIndex = 23;
-            this.SQLRadioButton.TabStop = true;
-            this.SQLRadioButton.Text = "SQL store";
-            this.SQLRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // AddConnection
-            // 
-            this.AddConnection.Location = new System.Drawing.Point(315, 119);
-            this.AddConnection.Name = "AddConnection";
-            this.AddConnection.Size = new System.Drawing.Size(82, 23);
-            this.AddConnection.TabIndex = 24;
-            this.AddConnection.Text = "Connections";
-            this.AddConnection.UseVisualStyleBackColor = true;
-            // 
-            // textBoxConnStr
-            // 
-            this.textBoxConnStr.Location = new System.Drawing.Point(152, 119);
-            this.textBoxConnStr.Name = "textBoxConnStr";
-            this.textBoxConnStr.Size = new System.Drawing.Size(140, 20);
-            this.textBoxConnStr.TabIndex = 26;
-            // 
-            // XMLRadioButton
-            // 
-            this.XMLRadioButton.AutoSize = true;
-            this.XMLRadioButton.Location = new System.Drawing.Point(27, 33);
-            this.XMLRadioButton.Name = "XMLRadioButton";
-            this.XMLRadioButton.Size = new System.Drawing.Size(73, 17);
-            this.XMLRadioButton.TabIndex = 19;
-            this.XMLRadioButton.Text = "XML store";
-            this.XMLRadioButton.UseVisualStyleBackColor = true;
-            // 
-            // LocationTxt
-            // 
-            this.LocationTxt.Location = new System.Drawing.Point(152, 59);
-            this.LocationTxt.Name = "LocationTxt";
-            this.LocationTxt.Size = new System.Drawing.Size(140, 20);
-            this.LocationTxt.TabIndex = 21;
-            this.LocationTxt.Text = "C:\\ComplexNetwork";
-            // 
-            // LabelConnStr
-            // 
-            this.LabelConnStr.AutoSize = true;
-            this.LabelConnStr.Location = new System.Drawing.Point(47, 123);
-            this.LabelConnStr.Name = "LabelConnStr";
-            this.LabelConnStr.Size = new System.Drawing.Size(88, 13);
-            this.LabelConnStr.TabIndex = 25;
-            this.LabelConnStr.Text = "ConnectionString";
-            // 
-            // location
-            // 
-            this.location.AutoSize = true;
-            this.location.Location = new System.Drawing.Point(48, 63);
-            this.location.Name = "location";
-            this.location.Size = new System.Drawing.Size(84, 13);
-            this.location.TabIndex = 20;
-            this.location.Text = "Storage location";
             // 
             // cancelButton
             // 
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(358, 189);
+            this.cancelButton.Location = new System.Drawing.Point(538, 194);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 19;
@@ -144,42 +57,175 @@
             // 
             // okButton
             // 
-            this.okButton.Location = new System.Drawing.Point(277, 189);
+            this.okButton.Location = new System.Drawing.Point(457, 194);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 18;
             this.okButton.Text = "OK";
             this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
             // 
-            // StorageSettings
+            // xmlBrowseButton
             // 
+            this.xmlBrowseButton.Location = new System.Drawing.Point(531, 35);
+            this.xmlBrowseButton.Name = "xmlBrowseButton";
+            this.xmlBrowseButton.Size = new System.Drawing.Size(82, 23);
+            this.xmlBrowseButton.TabIndex = 30;
+            this.xmlBrowseButton.Text = "Browse";
+            this.xmlBrowseButton.UseVisualStyleBackColor = true;
+            this.xmlBrowseButton.Click += new System.EventHandler(this.xmlBrowseButton_Click);
+            // 
+            // sqlRadioButton
+            // 
+            this.sqlRadioButton.AutoSize = true;
+            this.sqlRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sqlRadioButton.Location = new System.Drawing.Point(12, 130);
+            this.sqlRadioButton.Name = "sqlRadioButton";
+            this.sqlRadioButton.Size = new System.Drawing.Size(72, 17);
+            this.sqlRadioButton.TabIndex = 31;
+            this.sqlRadioButton.TabStop = true;
+            this.sqlRadioButton.Text = "SQL store";
+            this.sqlRadioButton.UseVisualStyleBackColor = true;
+            this.sqlRadioButton.CheckedChanged += new System.EventHandler(this.store_checkedChanged);
+            // 
+            // connectionsButton
+            // 
+            this.connectionsButton.Location = new System.Drawing.Point(531, 154);
+            this.connectionsButton.Name = "connectionsButton";
+            this.connectionsButton.Size = new System.Drawing.Size(82, 23);
+            this.connectionsButton.TabIndex = 32;
+            this.connectionsButton.Text = "Connections";
+            this.connectionsButton.UseVisualStyleBackColor = true;
+            this.connectionsButton.Click += new System.EventHandler(this.connectionsButton_Click);
+            // 
+            // connectionStrTxt
+            // 
+            this.connectionStrTxt.Location = new System.Drawing.Point(137, 157);
+            this.connectionStrTxt.Name = "connectionStrTxt";
+            this.connectionStrTxt.Size = new System.Drawing.Size(388, 20);
+            this.connectionStrTxt.TabIndex = 34;
+            // 
+            // xmlRadioButton
+            // 
+            this.xmlRadioButton.AutoSize = true;
+            this.xmlRadioButton.Location = new System.Drawing.Point(12, 12);
+            this.xmlRadioButton.Name = "xmlRadioButton";
+            this.xmlRadioButton.Size = new System.Drawing.Size(73, 17);
+            this.xmlRadioButton.TabIndex = 27;
+            this.xmlRadioButton.Text = "XML store";
+            this.xmlRadioButton.UseVisualStyleBackColor = true;
+            this.xmlRadioButton.CheckedChanged += new System.EventHandler(this.store_checkedChanged);
+            // 
+            // xmlOutputDirectoryTxt
+            // 
+            this.xmlOutputDirectoryTxt.Location = new System.Drawing.Point(137, 38);
+            this.xmlOutputDirectoryTxt.Name = "xmlOutputDirectoryTxt";
+            this.xmlOutputDirectoryTxt.Size = new System.Drawing.Size(388, 20);
+            this.xmlOutputDirectoryTxt.TabIndex = 29;
+            // 
+            // connectionStr
+            // 
+            this.connectionStr.AutoSize = true;
+            this.connectionStr.Location = new System.Drawing.Point(32, 161);
+            this.connectionStr.Name = "connectionStr";
+            this.connectionStr.Size = new System.Drawing.Size(88, 13);
+            this.connectionStr.TabIndex = 33;
+            this.connectionStr.Text = "ConnectionString";
+            // 
+            // xmlOutputDirectory
+            // 
+            this.xmlOutputDirectory.AutoSize = true;
+            this.xmlOutputDirectory.Location = new System.Drawing.Point(33, 42);
+            this.xmlOutputDirectory.Name = "xmlOutputDirectory";
+            this.xmlOutputDirectory.Size = new System.Drawing.Size(84, 13);
+            this.xmlOutputDirectory.TabIndex = 28;
+            this.xmlOutputDirectory.Text = "Output Directory";
+            // 
+            // txtBrowseButton
+            // 
+            this.txtBrowseButton.Location = new System.Drawing.Point(531, 94);
+            this.txtBrowseButton.Name = "txtBrowseButton";
+            this.txtBrowseButton.Size = new System.Drawing.Size(82, 23);
+            this.txtBrowseButton.TabIndex = 38;
+            this.txtBrowseButton.Text = "Browse";
+            this.txtBrowseButton.UseVisualStyleBackColor = true;
+            this.txtBrowseButton.Click += new System.EventHandler(this.txtBrowseButton_Click);
+            // 
+            // txtRadioButton
+            // 
+            this.txtRadioButton.AutoSize = true;
+            this.txtRadioButton.Location = new System.Drawing.Point(12, 71);
+            this.txtRadioButton.Name = "txtRadioButton";
+            this.txtRadioButton.Size = new System.Drawing.Size(72, 17);
+            this.txtRadioButton.TabIndex = 35;
+            this.txtRadioButton.Text = "TXT store";
+            this.txtRadioButton.UseVisualStyleBackColor = true;
+            this.txtRadioButton.CheckedChanged += new System.EventHandler(this.store_checkedChanged);
+            // 
+            // txtOutputDirectoryTxt
+            // 
+            this.txtOutputDirectoryTxt.Location = new System.Drawing.Point(137, 97);
+            this.txtOutputDirectoryTxt.Name = "txtOutputDirectoryTxt";
+            this.txtOutputDirectoryTxt.Size = new System.Drawing.Size(388, 20);
+            this.txtOutputDirectoryTxt.TabIndex = 37;
+            // 
+            // txtOutputDirectory
+            // 
+            this.txtOutputDirectory.AutoSize = true;
+            this.txtOutputDirectory.Location = new System.Drawing.Point(33, 101);
+            this.txtOutputDirectory.Name = "txtOutputDirectory";
+            this.txtOutputDirectory.Size = new System.Drawing.Size(84, 13);
+            this.txtOutputDirectory.TabIndex = 36;
+            this.txtOutputDirectory.Text = "Storage location";
+            // 
+            // StorageSettingsWindow
+            // 
+            this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 222);
+            this.CancelButton = this.cancelButton;
+            this.ClientSize = new System.Drawing.Size(624, 226);
+            this.Controls.Add(this.txtBrowseButton);
+            this.Controls.Add(this.txtRadioButton);
+            this.Controls.Add(this.txtOutputDirectoryTxt);
+            this.Controls.Add(this.txtOutputDirectory);
+            this.Controls.Add(this.xmlBrowseButton);
+            this.Controls.Add(this.sqlRadioButton);
+            this.Controls.Add(this.connectionsButton);
+            this.Controls.Add(this.connectionStrTxt);
+            this.Controls.Add(this.xmlRadioButton);
+            this.Controls.Add(this.xmlOutputDirectoryTxt);
+            this.Controls.Add(this.connectionStr);
+            this.Controls.Add(this.xmlOutputDirectory);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.okButton);
-            this.Controls.Add(this.storageGroupBox);
-            this.Name = "StorageSettings";
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "StorageSettingsWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "StorageSettings";
-            this.storageGroupBox.ResumeLayout(false);
-            this.storageGroupBox.PerformLayout();
+            this.Text = "Storage Settings";
+            this.Load += new System.EventHandler(this.StorageSettingsWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox storageGroupBox;
-        private System.Windows.Forms.Button Browse;
-        private System.Windows.Forms.RadioButton SQLRadioButton;
-        private System.Windows.Forms.Button AddConnection;
-        private System.Windows.Forms.TextBox textBoxConnStr;
-        private System.Windows.Forms.RadioButton XMLRadioButton;
-        private System.Windows.Forms.TextBox LocationTxt;
-        private System.Windows.Forms.Label LabelConnStr;
-        private System.Windows.Forms.Label location;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button xmlBrowseButton;
+        private System.Windows.Forms.RadioButton sqlRadioButton;
+        private System.Windows.Forms.Button connectionsButton;
+        private System.Windows.Forms.TextBox connectionStrTxt;
+        private System.Windows.Forms.RadioButton xmlRadioButton;
+        private System.Windows.Forms.TextBox xmlOutputDirectoryTxt;
+        private System.Windows.Forms.Label connectionStr;
+        private System.Windows.Forms.Label xmlOutputDirectory;
+        private System.Windows.Forms.Button txtBrowseButton;
+        private System.Windows.Forms.RadioButton txtRadioButton;
+        private System.Windows.Forms.TextBox txtOutputDirectoryTxt;
+        private System.Windows.Forms.Label txtOutputDirectory;
+        private System.Windows.Forms.FolderBrowserDialog browserDlg;
     }
 }
