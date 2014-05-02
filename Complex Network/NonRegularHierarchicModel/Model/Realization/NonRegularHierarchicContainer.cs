dca4ad4cc887ee8cf54656e239dcc067bcee074c
mika.samvelyan@gmail.com <mika.samvelyan@gmail.com@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-05-02 19:24:36 +0000

    Minor bug.

--- Complex Network/NonRegularHierarchicModel/Model/Realization/NonRegularHierarchicContainer.cs
+++ Complex Network/NonRegularHierarchicModel/Model/Realization/NonRegularHierarchicContainer.cs
@@ -52,7 +52,8 @@ namespace Model.NonRegularHierarchicModel.Realization
                     vertices = vertexCount;
                 }
                 return vertices;
-            }   
+            }
+            set { } 
         }
 
         public int BranchIndex
