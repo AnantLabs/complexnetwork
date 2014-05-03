0a45996a563f258d3c5276dfa11cf9d953a99090
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-05-03 17:35:45 +0000

    Changes in GUI.

--- trunk/Random Networks Explorer/Core/AbstractResearch.cs
+++ trunk/Random Networks Explorer/Core/AbstractResearch.cs
@@ -18,26 +18,31 @@ namespace Core
         protected ModelType modelType = ModelType.ER;
         protected int realizationCount = 1;
 
-        protected ResearchResult result;
+        private ManagerType managerType;
         protected AbstractEnsembleManager currentManager;
         protected delegate void ManagerRunner();
-        private ManagerType managerType;
+
+        protected ResearchResult result = new ResearchResult();       
 
         public AbstractResearch()
         {
             ResearchID = Guid.NewGuid();
-
-            // TODO read from config manager type.
-            managerType = ManagerType.Local;
-
             Status = Status.NotStarted;
+            GenerationType = GenerationType.Random;
 
             ResearchParameterValues = new Dictionary<ResearchParameter, object>();
             GenerationParameterValues = new Dictionary<GenerationParameter, object>();
             AnalyzeOption = AnalyzeOption.None;
 
-            InitializeParameters();
+            InitializeResearchParameters();
+            InitializeGenerationParameters();
+
+            managerType = Settings.WorkingMode;
         }
+
+        public Guid ResearchID { get; private set; }
+
+        public string ResearchName { get; set; }
 
         public ModelType ModelType
         {
@@ -46,19 +51,22 @@ namespace Core
             {
                 List<AvailableModelType> l = new List<AvailableModelType>((AvailableModelType[])this.GetType().GetCustomAttributes(typeof(AvailableModelType), true));
                 if (l.Exists(x => x.ModelType == value))
+                {
                     modelType = value;
+                    InitializeGenerationParameters();
+                }
                 else
                     throw new CoreException("Research does not support specified model type.");
             }
         }
 
-        public Guid ResearchID { get; private set; }
-
-        public string ResearchName { get; set; }
-
         public AbstractResultStorage Storage { get; set; }
 
+        public GenerationType GenerationType { get; set; }
+
         public string TracingPath { get; set; }
+
+        public Status Status { get; private set; }
 
         public int RealizationCount
         {
@@ -71,8 +79,6 @@ namespace Core
                     throw new CoreException("Realization count cannot be less then 1.");
             }
         }
-
-        public Status Status { get; private set; }
 
         public Dictionary<ResearchParameter, object> ResearchParameterValues { get; set; }
 
@@ -136,9 +142,32 @@ namespace Core
 
         private void InitializeParameters()
         {
+            ResearchParameterValues.Clear();
+            GenerationParameterValues.Clear();
+
             RequiredResearchParameter[] rp = (RequiredResearchParameter[])this.GetType().GetCustomAttributes(typeof(RequiredResearchParameter), true);
             for (int i = 0; i < rp.Length; ++i)
                 ResearchParameterValues.Add(rp[i].Parameter, null);
+
+            ModelTypeInfo info = ((ModelTypeInfo[])modelType.GetType().GetField(modelType.ToString()).GetCustomAttributes(typeof(ModelTypeInfo), false))[0];
+            Type t = Type.GetType(info.Implementation, true);
+            RequiredGenerationParameter[] gp = (RequiredGenerationParameter[])t.GetCustomAttributes(typeof(RequiredGenerationParameter), false);
+            for (int i = 0; i < gp.Length; ++i)
+                GenerationParameterValues.Add(gp[i].Parameter, null);
+        }
+
+        private void InitializeResearchParameters()
+        {
+            ResearchParameterValues.Clear();
+
+            RequiredResearchParameter[] rp = (RequiredResearchParameter[])this.GetType().GetCustomAttributes(typeof(RequiredResearchParameter), true);
+            for (int i = 0; i < rp.Length; ++i)
+                ResearchParameterValues.Add(rp[i].Parameter, null);
+        }
+
+        private void InitializeGenerationParameters()
+        {
+            GenerationParameterValues.Clear();
 
             ModelTypeInfo info = ((ModelTypeInfo[])modelType.GetType().GetField(modelType.ToString()).GetCustomAttributes(typeof(ModelTypeInfo), false))[0];
             Type t = Type.GetType(info.Implementation, true);
