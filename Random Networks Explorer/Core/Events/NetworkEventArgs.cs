03f58d608d0c4c257f7cd53e00f212492d188beb
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-06-24 17:36:51 +0000

    Temporary.

---
+++ Random Networks Explorer/Core/Events/NetworkEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Events
+{
+    public class NetworkEventArgs : EventArgs
+    {
+        public string Status { get; private set; }
+
+        public NetworkEventArgs(string status)
+        {
+            Status = status;
+        }
+    }
+
+    public delegate void NetworkStatusUpdateHandler(object sender, NetworkEventArgs e);
+}
