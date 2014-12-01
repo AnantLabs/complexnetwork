8b2379e0a3f6ceebe5a59df7acf2974187a217c8
ankocharyan <ankocharyan@7a4e6895-274d-bf2d-e384-cfbb0efd1f12>
2014-12-01 18:54:37 +0000

    Temporary implementation for Statistic Analyzer.

--- Random Networks Explorer/Session/StSessionManager.cs
+++ Random Networks Explorer/Session/StSessionManager.cs
@@ -13,12 +13,12 @@ namespace Session
 {
     public static class StSessionManager
     {
-        private static List<List<ResearchResult>> existingResults;
+        public static List<List<ResearchResult>> existingResultsByGroups;
         private static AbstractResultStorage storage;
 
         static StSessionManager()
         {
-            existingResults = new List<List<ResearchResult>>();
+            existingResultsByGroups = new List<List<ResearchResult>>();
             storage = CreateStorage();
         }
 
@@ -56,6 +56,56 @@ namespace Session
             StorageTypeInfo[] info = (StorageTypeInfo[])st.GetType().GetField(st.ToString()).GetCustomAttributes(typeof(StorageTypeInfo), false);
             Type t = Type.GetType(info[0].Implementation, true);
             return (AbstractResultStorage)t.GetConstructor(patametersType).Invoke(invokeParameters);
+        }
+
+        public static void SortByGroups()
+        {
+            List<ResearchResult> existingResults = storage.LoadAllResearchInfo();
+
+            while (existingResults.Count != 0)
+            {
+                List<ResearchResult> current = new List<ResearchResult>();
+                ResearchResult currentResult = existingResults[0];
+                current.Add(currentResult);
+                existingResults.Remove(currentResult);
+                int i = 0;
+                while(i < existingResults.Count())
+                {
+                    if (existingResults[i].ModelType == currentResult.ModelType &&
+                        existingResults[i].ResearchType == currentResult.ResearchType &&
+                        existingResults[i].Size == currentResult.Size &&
+                        SameParameters(existingResults[i], currentResult))
+                    {
+                        current.Add(existingResults[i]);
+                        existingResults.Remove(existingResults[i]);
+                    }
+                    else
+                    {
+                        ++i;
+                    }
+                }
+                existingResultsByGroups.Add(current);
+            }
+
+            return;
+        }
+
+        private static bool SameParameters(ResearchResult r1, ResearchResult r2)
+        {
+            // entadrum enq, te researchner@ nuyn model type-i ev nuyn research type-i en
+            foreach (GenerationParameter gp in r1.GenerationParameterValues.Keys)
+            {
+                if (r1.GenerationParameterValues[gp].ToString() != r2.GenerationParameterValues[gp].ToString())
+                    return false;
+            }
+
+            foreach (ResearchParameter rp in r1.ResearchParameterValues.Keys)
+            {
+                if (r1.ResearchParameterValues[rp].ToString() != r2.ResearchParameterValues[rp].ToString())
+                    return false;
+            }
+
+            return true;
         }
     }
 }
